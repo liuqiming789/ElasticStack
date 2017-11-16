9ef0cb53592bbe53a9eacd6aadf14cf3c1266743
Clark Xu <xu_chl@163.com>
2017-11-16 14:49:27 +0800

    2017-11-16

---
+++ Subject-项目开发/TMP/01-卸载x-pack.sh
@@ -0,0 +1,11 @@
+D:\installtools\ELK_ElasticStack\elasticsearch-5.4.3>bin\elasticsearch-plugin.bat remove x-pack
+-> removing [x-pack]...
+-> preserving plugin config files [D:\installtools\ELK_ElasticStack\elasticsearch-5.4.3\config\x-packed
+
+chcp 936
+
+
+启动kibana
+
+
+
