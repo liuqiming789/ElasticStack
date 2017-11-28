7df378fb529effd6ca740385477594ccb95fdd09
Clark Xu <xu_chl@163.com>
2017-11-28 16:11:31 +0800

    chef

--- Subject-LargeScaleElastic/Item-practice/02-Elasticsearch Puppet module.sh
+++ Subject-LargeScaleElastic/Item-practice/02-Elasticsearch Puppet module.sh
@@ -1,2 +1,16 @@
 Elasticsearch Puppet module
 https://github.com/elastic/puppet-elasticsearch
+
+一.模块的作用
+配置elasticsearch的实例,资源等.
+The module manages the following
+Elasticsearch repository files.
+Elasticsearch package.
+Elasticsearch configuration file.
+Elasticsearch service.
+Elasticsearch plugins.
+Elasticsearch templates.
+Elasticsearch ingest pipelines.
+Elasticsearch index settings.
+Elasticsearch Shield/X-Pack users, roles, and certificates.
+Elasticsearch keystores.
---
+++ Subject-LargeScaleElastic/Item-practice/03-Chef cookbook for Elasticsearch.sh
@@ -0,0 +1,5 @@
+https://github.com/elastic/cookbook-elasticsearch
+
+https://www.elastic.co/blog/deploying-elasticsearch-200-with-chef
+
+https://www.elastic.co/blog/deploying-elasticsearch-200-with-chef
